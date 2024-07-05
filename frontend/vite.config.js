1466d3d4039e93f6fac5d1e4b1cef5ed400ea8f7
Aditya Raj <143834549+Ranchi-Raj@users.noreply.github.com>
2024-07-05 12:34:43 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://erp-system-frontend.vercel.app',
+      '/api': 'https://erp-system-api.vercel.app',
        
     }
   }
