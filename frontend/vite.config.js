a17f4894227fcb42643c7871600e6896ee7bd218
Aditya Raj <143834549+Ranchi-Raj@users.noreply.github.com>
2024-07-06 15:15:52 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'https://erp-backend-xxnj.onrender.com/',
+      '/api': 'https://erp-backend-xxnj.onrender.com',
        
     }
   }
